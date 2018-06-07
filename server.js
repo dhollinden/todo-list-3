e8d768fd4a3f7cebf766bb9acba6c32154f52850
dhollinden <dave.hollinden@ithaka.org>
2018-06-07 08:05:02 -0400

    Delete route that was commented out

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express')
 var app = express()
 
-//app.get('/', function (req, res) {
-//    res.send(index.html)
-//})
-
 app.use(express.static('public'));
 
 app.listen(8000, function () {
